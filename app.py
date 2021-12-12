33ada8ae2d1b0307ffa0ba90c4911aac0c9c00b3
dv20000 <84127589+dv20000@users.noreply.github.com>
2021-12-12 20:32:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask_cors import cross_origin
 import sklearn
 import pickle
 import pandas as pd
+from pandas_datareader import data as pdr
+
+import yfinance as yf
+yf.pdr_override()
 
 app = Flask(__name__)
 model = pickle.load(open("flight_rf.pkl", "rb"))
